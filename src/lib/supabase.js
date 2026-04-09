fc6dc6f1d294d4f80b5298fce53a0d0727c704a5
veayra <83976659+veayra@users.noreply.github.com>
2026-04-09 21:24:39 +0100

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://fgfmmjazmhxkgdgtubba.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZm1tamF6bWh4a2dkZ3R1YmJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NjM4NTMsImV4cCI6MjA5MTMzOTg1M30.OkKSuRueVhT2EG4viSJ-xLpEoES2GpnBS7Fg2UZEX1k'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
